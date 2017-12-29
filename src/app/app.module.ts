7ec77b3a13f03d6cd0f783290463bf0e0276cabf
Jaroslaw Gryko <jaroslaw.gryko@gmail.com>
2017-12-29 23:58:19 +0100

    Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { AlertifyService } from './_services/alertify.service';
-import { AuthService } from './_services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { BsDropdownModule } from 'ngx-bootstrap';
-
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { AlertifyService } from './_services/alertify.service';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 
 @NgModule({
@@ -18,13 +22,17 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
 ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AuthService,
