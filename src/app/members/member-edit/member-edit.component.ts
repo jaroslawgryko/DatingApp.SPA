1daa1eaec5b9bb8a5c126ee3ba9a8f4d0896787d
Jaroslaw Gryko <jaroslaw.gryko@gmail.com>
2018-01-07 01:18:59 +0100

    member edit component 3

---
+++ src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,21 @@
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../_models/User';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  user: User;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+  }
+
+}
