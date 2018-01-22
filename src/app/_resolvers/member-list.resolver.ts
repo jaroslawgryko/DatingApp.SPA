11d4933e5ce307c778139645398a99b1d0647b46
Jaroslaw Gryko <jaroslaw.gryko@gmail.com>
2018-01-23 00:54:25 +0100

    Pagination in SPA

--- src/app/_resolvers/member-list.resolver.ts
+++ src/app/_resolvers/member-list.resolver.ts
@@ -1,18 +1,22 @@
 import { Observable } from 'rxjs/Rx';
 import { AlertifyService } from './../_services/alertify.service';
 import { UserService } from './../_services/user.service';
-import { Resolve, Router, RouterStateSnapshot, ActivatedRouteSnapshot } from "@angular/router";
-import { User } from "../_models/User";
-import { Injectable } from "@angular/core";
+import { Resolve, Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { User } from '../_models/User';
+import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
+
+    pageSize = 5;
+    pageNumber = 1;
+
     constructor(private userService: UserService, private router: Router,
         private alertify: AlertifyService) {}
 
         resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-            return this.userService.getUsers().catch(error => {
+            return this.userService.getUsers(this.pageNumber, this.pageSize).catch(error => {
                 this.alertify.error('Problem retrieving data');
                 this.router.navigate(['/members']);
                 return Observable.of(null);
--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -7,6 +7,8 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import { AuthHttp } from 'angular2-jwt';
+import { PaginatedResult } from '../_models/pagination';
+import { Response } from '@angular/http/src/static_response';
 
 
 
@@ -15,10 +17,26 @@ export class UserService {
     baseUrl = environment.apiUrl;
 constructor(private authHttp: AuthHttp) { }
 
-    getUsers(): Observable<User[]> {
-        return this.authHttp.get(this.baseUrl + 'users')
-            .map(response => <User[]>response.json())
-            .catch(this.handleError);
+    getUsers(page?: number, itemsPerPage?: number): Observable<User[]> {
+        const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
+        let queryString = '?';
+
+        if (page != null && itemsPerPage != null) {
+          queryString += 'pageNumber=' + page + '&pageSize=' + itemsPerPage;
+        }
+
+        return this.authHttp
+          .get(this.baseUrl + 'users' + queryString)
+          .map((response: Response) => {
+            paginatedResult.result = response.json();
+
+            if (response.headers.get('Pagination') != null) {
+              paginatedResult.pagination = JSON.parse(response.headers.get('Pagintion'));
+            }
+
+            return paginatedResult;
+          })
+          .catch(this.handleError);
     }
 
     private handleError(error: any) {
